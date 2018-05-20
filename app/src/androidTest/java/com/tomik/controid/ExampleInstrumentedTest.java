d3ece2616888886cdd3402bf17caf0cdb6a3336b
Tomasz Długokiński <tomaszdlugokinski@gmail.com>
2018-05-20 14:03:39 +0200

    Skok i kucnięcie w miarę zajebiście działa

--- app/src/androidTest/java/com/tomik/controid/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tomik/controid/ExampleInstrumentedTest.java
@@ -1,9 +1,8 @@
 package com.tomik.controid;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.*;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
@@ -11,11 +10,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -25,7 +22,7 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
@@ -38,35 +35,20 @@ public class ExampleInstrumentedTest {
             MainMenuActivity.class,
             true,     // initialTouchMode
             true);   // launchActivity. False to customize the intent
+
     @Test
     public void exportToFile10s() throws Exception {
         // Context of the app under test.
         MainMenuActivity mainMenuActivity = activityRule.getActivity();
-        //long startTime = System.currentTimeMillis();
         Log.i("TAG", "Test Start, idx = " + mainMenuActivity.IdxAll);
-        //while (startTime + 10000000 < System.currentTimeMillis()) ;
         TimeUnit.SECONDS.sleep(20);
         float[][] allReadingsAccel = mainMenuActivity.AllReadingsAccel;
-        float[][] allReadingsVel = mainMenuActivity.AllReadingsVel;
-        float[][] allReadingsPos = mainMenuActivity.AllReadingsPos;
+        float[][] allReadingsRot = mainMenuActivity.AllReadingsRot;
         int idxAll = mainMenuActivity.IdxAll;
         Log.i("TAG", "Test Stop, idx = " + idxAll);
-        /*File fileAccel = new File("C:/controid/przysp.txt");
-        File fileVel = new File("C:/controid/ped.txt");
-        File filePos = new File("C:/controid/pozycja.txt");
-        fileAccel.createNewFile();
-        fileVel.createNewFile();
-        filePos.createNewFile();*/
-        /*PrintWriter writerAccel = new PrintWriter("przysp.txt", "UTF-8");
-        PrintWriter writerVel = new PrintWriter("C:/controid/ped.txt", "UTF-8");
-        PrintWriter writerPos = new PrintWriter("C:/controid/pozycja.txt", "UTF-8");*/
         for (int i = 0; i < idxAll; i++) {
-            Log.i("ODCZYTACCEL",allReadingsAccel[i][0] + "," + allReadingsAccel[i][1] + "," + allReadingsAccel[i][2]);
-            Log.i("ODCZYTCEL",allReadingsVel[i][0] + "," + allReadingsVel[i][1] + "," + allReadingsVel[i][2]);
-            Log.i("ODCZYTPOS",allReadingsPos[i][0] + "," + allReadingsPos[i][1] + "," + allReadingsPos[i][2]);
+            Log.i("ODCZYTACCEL", allReadingsAccel[i][0] + "," + allReadingsAccel[i][1] + "," + allReadingsAccel[i][2]);
+            Log.i("ODCZYTROT", allReadingsRot[i][0] + "," + allReadingsRot[i][1] + "," + allReadingsRot[i][2]);
         }
-        /*writerAccel.close();
-        writerVel.close();
-        writerPos.close();*/
     }
 }
--- app/src/main/java/com/tomik/controid/MainMenuActivity.java
+++ app/src/main/java/com/tomik/controid/MainMenuActivity.java
@@ -15,41 +15,35 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+
 public class MainMenuActivity extends AppCompatActivity implements SensorEventListener {
 
     public TextView SensorLinearAccelerationTV;
-    //public TextView SensorSignificantMotionTV;
+    public TextView SensorSignificantMotionTV;
     private SensorManager mSensorManager;
     private Sensor mLinearAcceleration;
-    //private Sensor mRotationVector;
-    //private Sensor mMagneticField;
+    private Sensor mGameVector;
+    private long delay = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
-
         SensorLinearAccelerationTV = findViewById(R.id.AccelerometerTextID);
-        //SensorSignificantMotionTV = findViewById(R.id.SignificantMotionTextID);
+        SensorSignificantMotionTV = findViewById(R.id.SignificantMotionTextID);
         //do sensorów
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         //inicjalizacja czujników
         mLinearAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        //mRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        //mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        prevAccel = null;
+        mGameVector = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        for (int i = 0; i < maxMeasur; i++) {
+            LastRotation.add(new Float[]{0f, 0f, 0f});
+            LastMeasur.add(new Float[]{0f, 0f, 0f});
+        }
     }
 
     @Override
@@ -74,155 +68,194 @@ public class MainMenuActivity extends AppCompatActivity implements SensorEventLi
         return super.onOptionsItemSelected(item);
     }
 
-    static final float NS2S = 1.0f / 1000000000.0f;
-    float[] prevAccel = null;
-    float[] velocity = null;
-    float[] position = null;
-    float[] accel = null;
+    float[] accel = new float[3];
+    float[] accelOrg = new float[3];
+    float[] rotation = new float[3];
+    float[] startRotation = new float[3];
     boolean calibrationPressed = true;
-    float xCalibratedFix = 0;
-    float yCalibratedFix = 0;
-    float zCalibratedFix = 0;
-    long last_timestamp = 0;
-    private float[] mMagnetometerReading = new float[3];
-
     private final float[] mRotationMatrix = new float[9];
-    private final float[] mOrientationAngles = new float[3];
-    private final float accelKFactor = (float) 0.9;
 
     public int IdxAll = 0;
     public final int IdxAllMax = 100000;
     public float[][] AllReadingsAccel = new float[IdxAllMax][3];
-    public float[][] AllReadingsVel = new float[IdxAllMax][3];
-    public float[][] AllReadingsPos = new float[IdxAllMax][3];
     public float[][] AllReadingsRot = new float[IdxAllMax][3];
 
-    public int X=0;
-    public int Y=1;
-    public int Z=2;
+    public int X = 0;
+    public int Y = 1;
+    public int Z = 2;
     private int maxMeasur = 8;
     private int lastTmp = 0;
-    public float[][] LastMeasur = new float[maxMeasur][3];
+    private boolean useRotation = false;
+    public LinkedList<Float[]> LastMeasur = new LinkedList<>();
+    public LinkedList<Float[]> LastRotation = new LinkedList<>();
 
-    @SuppressLint({"SetTextI18n", "DefaultLocale"})
+    @SuppressLint({"DefaultLocale", "SetTextI18n"})
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
         switch (event.sensor.getType()) {
-            /*case Sensor.TYPE_ACCELEROMETER:
-                if (mMagnetometerReading==null)
-                    break;
-                SensorManager.getRotationMatrix(mRotationMatrix, null,
-                        event.values, mMagnetometerReading);
+            case Sensor.TYPE_LINEAR_ACCELERATION: {
+                System.arraycopy(event.values, 0, accelOrg, 0, 3);
 
-                // "mRotationMatrix" now has up-to-date information.
-
-                SensorManager.getOrientation(mRotationMatrix, mOrientationAngles);
-
-                SensorSignificantMotionTV.setText(String.format("%.2f", mOrientationAngles[0]) + " " +
-                        String.format("%.2f", mOrientationAngles[1]) + " " +
-                        String.format("%.2f", mOrientationAngles[2]));
-                break;*/
-            case Sensor.TYPE_LINEAR_ACCELERATION:
-
-                if (prevAccel == null) {
-                    prevAccel = new float[3];
-                    accel = new float[3];
-                    velocity = new float[3];
-                    position = new float[3];
-                    prevAccel[0] = prevAccel[1] = prevAccel[2] = 0f;
-                    velocity[0] = velocity[1] = velocity[2] = 0f;
-                    position[0] = position[1] = position[2] = 0f;
-                    break;
+                if (!useRotation) {
+                    System.arraycopy(accelOrg, 0, accel, 0, 3);
+                    if (rotation[Y] > 0) accel[Y] *= -1;
+                } else {
+                    accel[X] = (float) (accelOrg[X] * (Math.cos(rotation[X]) * Math.cos(rotation[Z])) +
+                            accelOrg[Y] * (Math.sin(rotation[Y]) * Math.sin(rotation[X])) +
+                            accelOrg[Z] * (-Math.sin(rotation[X]) * Math.cos(rotation[Y])) +
+                            accelOrg[Z] * (-Math.sin(rotation[X]) * Math.cos(rotation[Z])));
+                    accel[Y] = (float) (accelOrg[X] * (Math.sin(rotation[Y]) * -Math.sin(rotation[Z])) +
+                            accelOrg[Y] * Math.cos(rotation[Y]) +
+                            accelOrg[Z] * (Math.sin(rotation[Y]) * Math.cos(rotation[Z])));
+                    accel[Z] = (float) (accelOrg[X] * -Math.sin(rotation[X]) * Math.cos(rotation[Z]) +
+                            accelOrg[Y] * Math.sin(rotation[Y]) * Math.cos(rotation[X]) +
+                            accelOrg[Z] * Math.cos(rotation[X]) * Math.cos(rotation[Z]) +
+                            accelOrg[Z] * -Math.sin(rotation[X]) * Math.cos(rotation[Z]));
                 }
 
-                float dt = (event.timestamp - last_timestamp) * NS2S;
-                last_timestamp = event.timestamp;
-                if (calibrationPressed) {
-                    Calibrate(event.values, dt);
-                    IdxAll = 0;
-                    break;
-                }
+                float sum = 0;
+                for (float v : accelOrg) sum += v;
+                for (float v : accel) sum -= v;
 
-                for (int index = 0; index < 3; ++index) {
-                    accel[index] = event.values[index];
+                for (int index = 0; index < accel.length; ++index)
+                    if (Math.abs(accel[index]) < 1.2)
+                        accel[index] = 0;
 
-                    //eliminacja drgań
-                    if (Math.abs(accel[index])<1.2)
-                        accel[index]=0;
-                    //if (Math.abs(accel[index])>10)
-                    //    accel[index]=10;
-                    AllReadingsAccel[IdxAll % IdxAllMax][index] = accel[index];
-
-                    float last_velocity = velocity[index];
-                    velocity[index] += (accel[index] + prevAccel[index]) / 2 * dt;
-                    AllReadingsVel[IdxAll % IdxAllMax][index] = velocity[index];
-                    position[index] += (velocity[index] + last_velocity) / 2 * dt;
-                    AllReadingsPos[IdxAll % IdxAllMax][index] = position[index];
-                    prevAccel[index] = accel[index];
-                }
+                System.arraycopy(accel, 0, AllReadingsAccel[IdxAll % IdxAllMax], 0, accel.length);
+                System.arraycopy(rotation, 0, AllReadingsRot[IdxAll % IdxAllMax], 0, rotation.length);
                 IdxAll++;
-                SensorLinearAccelerationTV.setText(String.format("%.2f", position[0]) + " " +
-                        String.format("%.2f", position[1]) + " " +
-                        String.format("%.2f", position[2]));
 
-
-                for (int i=0;i<lastTmp;i++)
-                    for (int j=0;j<3;j++)
-                        LastMeasur[i][j] = LastMeasur[i+1][j];
-                for (int j=0;j<3;j++)
-                    LastMeasur[lastTmp][j] = accel[j];
-                if (lastTmp<maxMeasur-1) lastTmp++;
+                //zapisz ostatnie 10 odczytów (używane do analizy zdarzeń)
+                LastRotation.add(new Float[]{rotation[X], rotation[Y], rotation[Z]});
+                LastMeasur.add(new Float[]{accel[X], accel[Y], accel[Z]});
+                LastRotation.removeFirst();
+                LastMeasur.removeFirst();
+                //wykryj zdarzenia
                 DetectMoves();
 
+                //wypisanie na ekran
+                SensorLinearAccelerationTV.setText(String.format("%+.2f", accel[X]) + " " +
+                        String.format("%+.2f", accel[Y]) + " " +
+                        String.format("%+.2f", accel[Z]) + " " +
+                        String.format("%+.2f", sum));
                 break;
-                case Sensor.TYPE_GYROSCOPE:
+            }
+            case Sensor.TYPE_GAME_ROTATION_VECTOR:
+                if (rotation == null)
                     break;
+                if (delay > 0)
+                    calibrationPressed = true;
+                updateOrientation(event.values);
+                SensorSignificantMotionTV.setText(String.format("%+.2f", rotation[X]) + " " +
+                        String.format("%+.2f", rotation[Y]) + " " +
+                        String.format("%+.2f", rotation[Z]));
+                break;
             default:
                 break;
         }
-        // Do something with this sensor value.
+    }
+
+    private void updateOrientation(float[] values) {
+        SensorManager.getRotationMatrixFromVector(mRotationMatrix, values);
+        SensorManager.getOrientation(mRotationMatrix, rotation);
+        if (calibrationPressed) {
+            //System.arraycopy(rotation, 0, startRotation, 0, rotation.length);
+            for (int i = 0; i < 3; i++)
+                startRotation[i] = 0;
+            calibrationPressed = false;
+            Log.i("ZAKTUALIZOWANO ROTACJĘ", "!!!!!!!!!!!!!!!!!!!!!");
+            delay--;
+        }
+        for (int i = 0; i < rotation.length; i++) {
+            rotation[i] -= startRotation[i];
+            if (rotation[i] > Math.PI)
+                rotation[i] -= Math.PI * 2;
+            if (rotation[i] <= -Math.PI)
+                rotation[i] += Math.PI * 2;
+        }
     }
 
     private void DetectMoves() {
         if (checkForJump())
-            Log.i("SKOK","!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            Log.i("SKOK", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (checkForCrouch())//skok przed kucaniem
+            Log.i("KUCNIĘCIE", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (checkForLeft())
+            Log.i("LEWO", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (checkForRight())
+            Log.i("PRAWO", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
-    private final int jumpCooldown = 4;
-    private int lastJump=0;
-    private boolean checkForJump() {
-        if (lastJump>0) {
-            lastJump--;
+    private boolean checkForRight() {
+        return checkForSide(false);
+    }
+
+    private boolean checkForLeft() {
+        return checkForSide(true);
+    }
+
+    private final int sideCooldown = 4;
+    private int lastSide = 0;
+
+    private boolean checkForSide(boolean sign) {
+        if (lastJump > 0 || lastCrouch > 0)
+            return false;
+        if (lastSide > 0) {
+            lastSide--;
             return false;
         }
-        if ((LastMeasur[maxMeasur-1][Y]>6 || LastMeasur[maxMeasur-2][Y]>6) && (Math.abs(LastMeasur[maxMeasur-3][Y])<2.5 || LastMeasur[maxMeasur-2][Y]<2.5) &&
-                !(LastMeasur[maxMeasur-2][Z]<-5.5 || LastMeasur[maxMeasur-3][Z]<-5.5)) {
-            lastJump = jumpCooldown;
+        int s;
+        if (sign)
+            s = -1;
+        else
+            s = 1;
+        if (LastMeasur.get(maxMeasur - 1)[X] * s > 2.5 && LastMeasur.get(maxMeasur - 2)[X] * s < 2.5)
+        //Math.abs(LastMeasur[maxMeasur - 2][X]) == 0)
+        //&& !(LastMeasur[maxMeasur-2][X]<-5.5 || LastMeasur[maxMeasur-3][X]<-5.5))
+        {
+            lastSide = sideCooldown;
             return true;
         }
         return false;
     }
 
-    float xCal = 0;
-    float yCal = 0;
-    float zCal = 0;
-    float calibrationTime = 0;
-    final float calibrationTimeLimit = 10;
+    private final int crouchCooldown = 6;
+    private int lastCrouch = 0;
 
-    private void Calibrate(float[] acceleration, float dt) {
-        xCal += acceleration[0];
-        yCal += acceleration[1];
-        zCal += acceleration[2];
-        calibrationTime += dt;
-        //if (calibrationTime < calibrationTimeLimit)
-        //    return;
-        calibrationPressed = false;
-        xCalibratedFix = xCal / calibrationTime;
-        yCalibratedFix = yCal / calibrationTime;
-        zCalibratedFix = zCal / calibrationTime;
-        prevAccel = null;
+    private boolean checkForCrouch() {
+        if (lastJump > 0)
+            return false;
+        if (lastCrouch > 0) {
+            lastCrouch--;
+            return false;
+        }
+        if (RotationChange() < 0.65) return false;
+        lastCrouch = crouchCooldown;
+        return true;
+    }
+
+    private final int jumpCooldown = 4;
+    private int lastJump = 0;
+
+    private boolean checkForJump() {
+        if (lastJump > 0) {
+            lastJump--;
+            return false;
+        }
+        if (Math.abs(LastMeasur.get(maxMeasur - 1)[Y]) < 4.5 && Math.abs(LastMeasur.get(maxMeasur - 2)[Y]) < 4.5)
+            return false;
+        lastJump = jumpCooldown;
+        return true;
+    }
+
+    float RotationChange() {
+        float minY = 1000;
+        float maxY = -1;
+        for (int i = LastRotation.size() - 1; LastRotation.size() - 1 - i < 4; i--) {
+            minY = Math.min(minY, LastRotation.get(i)[Y]);
+            maxY = Math.max(maxY, LastRotation.get(i)[Y]);
+        }
+        return Math.abs(maxY - minY);
     }
 
     @Override
@@ -234,9 +267,7 @@ public class MainMenuActivity extends AppCompatActivity implements SensorEventLi
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mLinearAcceleration, SensorManager.SENSOR_DELAY_NORMAL);
-
-        //mSensorManager.registerListener(this, mRotationVector, SensorManager.SENSOR_DELAY_NORMAL);
-        //mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGameVector, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -248,9 +279,5 @@ public class MainMenuActivity extends AppCompatActivity implements SensorEventLi
 
     public void Calibration(View view) {
         calibrationPressed = true;
-        xCal = 0;
-        yCal = 0;
-        zCal = 0;
-        calibrationTime = 0;
     }
 }
